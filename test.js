1b73817d56109e8c0fea573f68cf022ca791958a
Maarten Coppens <martinuske@gmail.com>
2022-09-12 20:08:04 -0300

    remove redundant comment

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 const ArkAdapter = require('./index.js');
 
-// warfare grocery replace donor park void begin math woman latin body life
 /**
  * DRzgcj97d3hFdLJjYhPTdBQNVeb92mzrx5 warfare grocery replace donor park void begin math woman latin body life
  * DRFp1KVCuCMFLPFrHzbH8eYdPUoNwTXWzV First signature: eternal shrimp catch pause giraffe yard hat day pull august brush sign apple strategy clutch animal heavy escape car walk juice umbrella pluck must
