8bd697a6b07f9e991c1f495749b2c9496dc147ee
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2022-09-14 23:49:10 +0200

    Cleanup

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const { Identities, Transactions } = require('@arkecosystem/crypto');
+const { Identities, Transactions, Managers } = require('@arkecosystem/crypto');
 const bip39 = require('bip39');
 
 const DEFAULT_API_MAX_PAGE_SIZE = 100;
@@ -9,40 +9,70 @@ class ArkAdapter {
   constructor(options) {
     this.apiURL = options.apiURL || DEFAULT_API_URL;
     this.apiMaxPageSize = options.apiMaxPageSize || DEFAULT_API_MAX_PAGE_SIZE;
+
+    // Needs to be set to a height which supports version 2 transactions.
+    Managers.configManager.setHeight(20000000);
   }
 
   async connect({ passphrase }) {
     this.passphrase = passphrase;
     this.address = Identities.Address.fromPassphrase(passphrase);
     this.publicKey = Identities.PublicKey.fromPassphrase(passphrase);
-    this.nonce = await this.getNonce({ address: this.address });
+    this.nonce = null;
   }
 
   async disconnect() {}
 
+  async updateNonce() {
+    try {
+      const accountNonce = await this.getNonce({ address: this.address })
+      if (this.nonce == null || accountNonce > this.nonce) {
+        this.nonce = accountNonce;
+      }
+    } catch (error) {
+      if (
+        error.name === 'AccountNotFoundError' ||
+        (error.response && error.response.status === 404)
+      ) {
+        this.nonce = 0;
+      } else {
+        throw error;
+      }
+    }
+  }
+
   async createTransfer({ amount, recipientAddress, message, fee, timestamp }) {
+    await this.updateNonce();
     const transferBuilder = Transactions.BuilderFactory.transfer();
 
     const transaction = transferBuilder
       .version(2)
-      .nonce(this.nonce)
+      .nonce(++this.nonce)
       .amount(amount)
       .fee(fee)
       .senderPublicKey(this.publicKey)
       .recipientId(recipientAddress)
       // Convert unix to epoch
-      .timestamp((timestamp == null ? Date.now() : timestamp) / 1000)
+      .timestamp(Math.round((timestamp == null ? Date.now() : timestamp) / 1000))
       .vendorField(message)
       .build();
 
     const privateKey = Identities.PrivateKey.fromPassphrase(this.passphrase);
 
-    Transactions.Signer.sign(transaction, {
+    Transactions.Signer.sign(transaction.data, {
       publicKey: this.publicKey,
       privateKey,
     });
 
-    return transaction;
+    // Serialize it for the Ark DEX adapter.
+    transaction.data.senderAddress = transaction.data.senderId;
+    transaction.data.recipientAddress = transaction.data.recipientId;
+    transaction.data.amount = transaction.data.amount.toString();
+    transaction.data.fee = transaction.data.fee.toString();
+    transaction.data.message = transaction.data.vendorField || '';
+    transaction.data.nonce = transaction.data.nonce.toString();
+
+    return transaction.data;
   }
 
   async createWallet() {
@@ -63,21 +93,31 @@ class ArkAdapter {
   }
 
   async postTransaction({ transaction }) {
-    await axios.post(`${this.apiURL}/transactions`, transaction);
+    await axios.post(`${this.apiURL}/transactions`, { transactions: [transaction] });
   }
 
   async getAccountNextKeyIndex({ address }) {
-    return await this.getNonce({ address });
+    return this.getNonce({ address });
   }
 
   async getAccountBalance({ address }) {
-    return (await axios.get(`${this.apiURL}/wallets?address=${address}`)).data
-      .data[0].balance;
+    const account = await this.getAccount(address);
+    return account.balance;
+  }
+
+  async getAccount(address) {
+    const account = (await axios.get(`${this.apiURL}/wallets?address=${address}`)).data.data[0];
+    if (!account) {
+      const error = new Error(`Failed to fetch account with address ${address}`);
+      error.name = 'AccountNotFoundError';
+      throw error;
+    }
+    return account;
   }
 
   async getNonce({ address }) {
-    return (await axios.get(`${this.apiURL}/wallets/${address}`)).data.data
-      .nonce;
+    const account = await this.getAccount(address);
+    return Number(account.nonce || '0');
   }
 }
 
